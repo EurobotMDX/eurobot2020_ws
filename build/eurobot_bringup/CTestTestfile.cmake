037ce1d79cdcfa57d0702f9a13e395ce223f71f8
simonklimek <simonklimek91@gmail.com>
2020-02-07 17:18:25 +0000

    turtlebot teleop keyboard

---
+++ build/eurobot_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros/lidar_ws/src/eurobot_bringup
+# Build directory: /home/ros/lidar_ws/build/eurobot_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
